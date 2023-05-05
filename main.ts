0994b45c9cc4dd38f48ff099f388645d49e3b984
aidr669 <132546582+aidr669@users.noreply.github.com>
2023-05-05 12:25:06 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
-})
+basic.showString("hola")
+for (let index = 0; index < 5; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showString("adios")
+}
